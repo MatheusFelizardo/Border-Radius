33a816d20b10164d718873b655159c799f4585d3
Matheus Felizardo <matheus.felizardo2@gmail.com>
2020-05-30 23:48:48 -0300

    Add files via upload
    Diminuição do código do script

--- script.js
+++ script.js
@@ -11,17 +11,12 @@ let inferiorDireito = document.getElementById("inferiorDireito").value
     document.getElementById("caixa").style.borderBottomLeftRadius = `${inferiorEsquerdo}%`;
     document.getElementById ("caixa").style.borderBottomRightRadius = `${inferiorDireito}%`;
 
-}
-
-/* ATUALIZA OS RESULTADOS NO TEXTO HTML EM TEMPO REAL*/
-function mostrarResultado () {
-let superiorEsquerdo = document.getElementById("superiorEsquerdo").value
-let superiorDireito = document.getElementById("superiorDireito").value
-let inferiorEsquerdo = document.getElementById("inferiorEsquerdo").value
-let inferiorDireito = document.getElementById("inferiorDireito").value
-
-
- let resultado = document.getElementById ("resultado")
-resultado.innerHTML = `Top-Left: ${superiorEsquerdo} % <br/> 
- Top-Right: ${superiorDireito}% <br/> Bottom-Left: ${inferiorEsquerdo}% <br/> Bottom-Right: ${inferiorDireito}%`
+    // ATUALIZA OS RESULTADOS NO TEXTO HTML EM TEMPO REAL
+    function mostrarResultado () {
+        
+         let resultado = document.getElementById ("resultado")
+        resultado.innerHTML = `Top-Left: ${superiorEsquerdo} % <br/> 
+         Top-Right: ${superiorDireito}% <br/> Bottom-Left: ${inferiorEsquerdo}% <br/> Bottom-Right: ${inferiorDireito}%`
+        }
+    return mostrarResultado ()
 }
